da161cad75234baa02a084f4ec7904d973ee7aed
acarolinacc <acarolinacc2003@gmail.com>
2022-12-10 11:43:44 +0000

    First Commit

---
+++ src/main/java/pt/up/fe/ldts/example2/Circle.java
@@ -0,0 +1,10 @@
+package pt.up.fe.ldts.example2;
+
+public class Circle extends Shape{
+    public Circle(double x, double y, double radius) {
+        super(x,y);
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/main/java/pt/up/fe/ldts/example2/Rectangle.java
@@ -0,0 +1,8 @@
+package pt.up.fe.ldts.example2;
+
+public class Rectangle extends Shape{
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+}
